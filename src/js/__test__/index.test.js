1910303e0fee149c41c411fe5eaaa05329d0c9c4
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2018-02-15 10:50:59 +0000

    update example test

--- src/js/__test__/index.test.js
+++ src/js/__test__/index.test.js
@@ -1,10 +1,17 @@
 import Greeting from '../index.js';
 
-describe('Index', () => {
-    test('sayHello()', () => {
-        let greeting = new Greeting();
-        let message = greeting.sayHello('John');
+describe('Greeting', () => {
+    describe('sayHello()', () => {
+        test('it should exist', () => {
+            let greeting = new Greeting();
+            expect(greeting.sayHello).not.toBe(undefined);
+        });
 
-        expect(message).toBe('Hi John');
+        test('it should return message', () => {
+            let greeting = new Greeting();
+            let message = greeting.sayHello('John');
+
+            expect(message).toBe('Hi John');
+        });
     });
 });
